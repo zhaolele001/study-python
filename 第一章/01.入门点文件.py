4179f50b53027f3d4becb3d79ee5204567abfd00
zhaolele <2743559351@qq.com>
2026-03-27 19:54:56 +0800

    git初始化操作

--- 第一章/01.入门点文件.py
+++ 第一章/01.入门点文件.py
@@ -1,5 +1,6 @@
 # 需求：控制台输出 hello python hello word
+# git 强制将远程更新到本能地git pull origin master --allow-unrelated-histories
+# git 强制推送git push --set-upstream origin master -f
 print("hello world")
 print("hello python")
 print("------------------")
-  
