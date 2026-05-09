406ba5a0c74367ab3dd1a16660a9665543354346
“zll” <2743559351@qq.com>
2026-05-09 16:21:45 +0800

    网络编程基础

--- 进阶/17.创建socket对象.py
+++ 进阶/17.创建socket对象.py
@@ -5,15 +5,12 @@
 import socket
 #参数1：address  IAPV4/IPV6
 #参数2：TCP/UDP
-socketObjServe = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-socketObjServe.bind(('127.0.0.1', 8888))
-socketObjServe.listen(5)
-accept = socketObjServe.accept()
-print(accept)
-
 socketObjClient = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socketObjClient.connect(('127.0.0.1', 8888))
+data = socketObjClient.recv(1024).decode("utf-8")
+print(f"服务器端信息：{data}")
+socketObjClient.send(b"服务器你好")
+
 
 
 
--- 进阶/17.网络编程服务端.py
+++ 进阶/17.网络编程服务端.py
@@ -10,3 +10,14 @@
     7.释放资源
 """
 import socket
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(('127.0.0.1', 0))
+server_socket.listen(5)
+print(1)
+accept_socket, client_info = server_socket.accept()
+print(2)
+data = accept_socket.send(b"Welcome to the server!")
+print(f"服务器端收到：，来自{client_info} 的信息{data}")
+accept_socket.recv(1024).decode("utf-8")
+# accept_socket.close()
